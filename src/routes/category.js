1fd691a77986b4acffaa79c9e20d87fe1431f5f8
namheheh <105479816+namheheh@users.noreply.github.com>
2023-09-28 06:46:35 +0700

    middlewares product -product detail - category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createCategory, getAllCategories, getCategory, updateCategory, removeCategory } from '../controller/category'
+import { checkPermission } from '../middleware/checkPermission';
 
 const categoryRouter = express.Router();
 
-categoryRouter.post('/category/add', createCategory);
+categoryRouter.post('/category/add',checkPermission,  createCategory);
 categoryRouter.get('/category', getAllCategories);
 categoryRouter.get('/category/:id', getCategory);
-categoryRouter.put('/category/:id/update', updateCategory);
-categoryRouter.delete('/category/:id', removeCategory);
+categoryRouter.put('/category/:id/update',checkPermission,  updateCategory);
+categoryRouter.delete('/category/:id',checkPermission,  removeCategory);
 
 export default categoryRouter;
