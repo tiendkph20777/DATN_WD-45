ac17e79c973101e9a786b11807ea6e1018302ae5
Zinh11 <vinh11.dev@gmail.com>
2023-10-23 09:58:11 +0700

    connect cloud.mongodb.com

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use("/api", authRouter)
 app.use("/api", roleRouter)
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/DATN", {
+mongoose.connect("mongodb+srv://laxus:0e5eEKo4gYkGAgYH@cluster0.yfuqvhg.mongodb.net/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
