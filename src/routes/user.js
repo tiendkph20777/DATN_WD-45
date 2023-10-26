4332e46a06d06fb43be2cf31c1a7a6822e1c5852
namheheh <105479816+namheheh@users.noreply.github.com>
2023-10-27 04:16:32 +0700

    up_user

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ import { checkPermission } from '../middleware/checkPermission';
 
 const userRouter = express.Router();
 
-userRouter.get('/user', checkPermission, getAllUsers)
+userRouter.get('/user', getAllUsers)
 userRouter.get('/user/:id', checkPermission, getOneUserById)
 userRouter.post('/user/add/staff', checkPermission, createStaff)
 userRouter.put('/user/:id/update', checkPermission, updateUser)
